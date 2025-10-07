f5da26a7fbfb5e0d7ed9d7037d5259573abab087
Allen Ng <ng.allen@metlife.co.jp>
2025-10-07 15:14:14 +0900

    fix: release

--- version/version.go
+++ version/version.go
@@ -8,7 +8,7 @@ import (
 )
 
 var (
-	Version            = "2.5.1"
+	Version            = "2.5.3"
 	VersionPrerelease  = "dev"
 	VersionMetadata    = ""
 	AzurePluginVersion = version.NewPluginVersion(Version, VersionPrerelease, VersionMetadata)
